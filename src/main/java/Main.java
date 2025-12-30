7c2bd4de686b3f4c68e61996429d54137c2d63ed
wakeupnoe <moritz.schwalm@gmx.de>
2025-12-30 23:19:27 +0100

    fixxxx

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -32,7 +32,7 @@ public class Main {
      * TODO: Write a method to calculate the factorial of a number.
      */
     public static int factorial(int n) {
-        if(n==1) return 1;
+        if(n==0) return 1;
         return n*factorial(n-1);
     }
 }
